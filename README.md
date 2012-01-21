4a904f3a5d662366be3e9fcb1139c2064ff6501c
Tor Valamo <tor.valamo@gmail.com>
2012-01-21 03:26:41 +0100

    started 0.3 branch with a fresh rewrite

--- README.md
+++ README.md
@@ -1,137 +1,42 @@
 # argtype.js
 
-JavaScript function arguments type checker.
+Completely rewritten. Old docs no longer relevant, more will come here.
 
-## Install and init
+Example of use:
 
-### Browser
+    var func = argtype(this, ['?b', true, 'n=[-9,9]', 'f'],
+               function(aBool, aNum, aFunc) {
+                 /* stuff here */
+               })
 
-Use the `argtype.js` file.
+* The first (optional) argument is a boolean with default value true.
+* The second (required) argument must be a number (int or float) between -9 and +9 (inclusive).
+* The third (required) argument is a function.
 
-#### !! Quirk !!
+Putting it short:
 
-If at one point in your script you are creating a global
-object called 'module', you need to make sure that `argtype.js` is initialized
-BEFORE that module object is created, otherwise `argtype.js` will modify it.
+* `?` means optional, and must be the first character of the argtype string. `'?s'` - optional string.
+* `0` means its value can be null. This must be the next character (first if no optional). `'0s'` - required string, but can be null
+* `+` means that the value must be either an unsigned (positive) number, or case-sensitivity in a regex part (see below).
+* Then follows the type character. There are many different types:
+** `.` - Untyped (the type of the input is not checked, just optionality and default values)
+** `n` - Number. Use `i` for integer and `f` for float to be specific.
+** `s` - String.
+** `b` - Boolean.
+** `c` - Function. The input must match typeof 'function'
+** `o` - Object. An object that matches typeof 'object'. Function-objects will not match (see `t`)
+** `a` - Array. Input matches instanceof Array.
+** `t` - Function-object. Input matches instanceof the function-object given in the next argument.
+* After the type character comes range limits and/or pattern matching. Only some types support either.
+** `/` indicates a regular expression follows until the end of the string. This will be matched against untyped or string inputs. It will be ignored for any other types.
+** `=` indicates that the value is within a range, given as a closed interval `[from,to]` or an open interval `(from,to)`. Bracket types can be mixed, like so `[closed,open)`.
+** `>`, `>=`, `<`, `<=` indicates that the value should be 'greater than', 'greater than or equal to', 'lesser than' or 'lesser than or equal to' the number which follows. `s>3` - A string longer than 3 characters. Length also works for arrays. Numbers use their actual value.
 
-    <script src="argtype.js"></script>
-    <script src="script-that-makes-a-module-object.js"></script>
+The next argument can be (in the case of 't') further specification of the string definition, and must be the function-object which input will be matched (instanceof) against.
 
-### For Node.js
-
-Using [npm](http://npmjs.org)
-
-    npm install argtype
-
-Then in your app
-
-    var argtype = require('argtype')
-
-## Details
-
-Both these forms are accepted, so whatever feels prettier at any given time
-can be used ;)
-
-### argtype(thisObj, types, function)
-### argtype(thisObj, function, types)
-
-- `thisObj` is what is referenced by 'this' inside the function
-- `types` is an array with the format shown below
-- `function` is the function that will be wrapped
-
-Returns a function that wraps the original. Use as normal.
-
-### Types array
-
-The types array consists of one entry per 'checked' argument (any arguments
-past the length of the types array will not be checked).
-
-Each entry can be one of three different styles:
-
-1. `type`
-2. `[optional type]`
-3. `[optional type, default value]`
-
-### Valid types
-
-* Any JavaScript type which can be matched with `typeof`, __except
-'undefined'__. Examples include 'object', 'number', 'string', 'regexp',
-'function', 'boolean' and 'array' (array is not really a primitive type, but it
-is handled separately as if it was).
-* Types 'integer' or 'float' are interpreted as 'number'
-* If the type is an actual RegExp objects (instead of just the string 'regexp'),
-the value will be evaluated with the regex. This is useful if you want to make
-sure the input is of a specific format.
-
-## Use
-
-Wrap an already defined function
-
-    function formatAddress(number, street, city, zip) {
-      return (number ? number + ', ' : '') + street + '\n'
-           + city + ' - ' + zip
-    }
-    
-    formatAddress = argtype(this, formatAddress, [['number'], 'string', 'string', /^\d{5}$/])
-
-Or just use a closure
-
-    var formatAddress = argtype(this, [['number'], 'string', 'string', /^\d{5}$/],
-                                function(number, street, city, zip) {
-      return (number ? number + ', ' : '') + street + '\n'
-           + city + ' - ' + zip
-    })
-
-Now it will complain if we pass it something bad
-
-    > formatAddress(123, 'JSVille', '12345')
-    Error: Function argument 4 got undefined, expected /^\d{5}$/.
-    > formatAddress(123, 456, 'JSVille', '12345')
-    Error: Function argument 2 got number, expected string.
-
-### Weird errors?
-
-Sometimes the errors can be a little bit misleading, but just check your
-input format against your argtypes definition and you'll probably spot it.
-
-In the following snippet the last argument doesn't match the regex of 5 digits.
-
-    > formatAddress('Main Street', 'JSVille', 123)
-    Error: Function argument 3 got number, expected /^\d{5}$/.
-    > formatAddress('Main Street', 'JSVille', '123')
-    Error: Function argument 3 got string, expected /^\d{5}$/.
-
-Below it doesn't find a first argument (number), because it is in
-quotes, so it thinks that it belongs to the second argument, seeing as the
-first argument is optional. This one can be very tricky to spot, but you're
-using a __type checker__ for a reason. Get your types right! ;)
-
-    > formatAddress('123', 'Main Street', 'JSVille', '12345')
-    Error: Function argument 3 got string, expected /^\d{5}$/.
-
-## Beware!
-
-You cannot have an optional argument before another argument of the same type.
-
-    > func = argtype(this, function(){}, [['number'], 'number'])
-    > func(4)
-    Error: Function argument 2 got undefined, expected number.
-
-This will make it think that the optional argument was in fact specified, and
-that the non-optional argument is missing!
-
-It could be worked around with a bit of effort, but there will always be corner
-cases.
-
-If you have a lot of optional arguments, you should consider putting them in an
-options hash object. This has the advantage that same-type optional arguments
-are unlimited.
-
-    > func(something, something else, {
-      'option1': 'value',
-      'option2': 'another'
-    })
+It can also be the default value for an optional argument, or in the case of 'o' (object) it can contain default attribute values.
+A 't' type can also have an optional argument, but then as the second argument after the definition (following the function-object).
 
 ## License
 
-See the file `COPYING`.
+See the file `LICENSE`.
--- argtype.js
+++ argtype.js
@@ -6,77 +6,219 @@
  * See file COPYING for details.
  */
 
-function argtypeCheck(value, type) {
-  if (value == undefined) {
-    return false
-  } else if (type instanceof RegExp) {
-    // type can be a format check regex
-    // for instance /^[a-z]+$/ to ensure only lower case a-z
-    return type.test(value)
-  } else if (type == 'regexp') {
-    return value instanceof RegExp
-  } else if (type == 'array') {
-    return value instanceof Array
-  } else {
-    if (type == 'integer' || type == 'float') type = 'number'
-    return typeof value == type
+function __typeof(arg) {
+  return typeof arg == this.ctype
+}
+
+function __length(arg) {
+  arg = this.ctype == 'number' ? arg : arg.length
+  if (this.low) {
+    if (this.low[0] == 'gt' && arg <= this.low[1]) return false
+    else if (arg < this.low[1]) return false
   }
+  if (this.high) {
+    if (this.high[0] == 'lt' && arg >= this.high[1]) return false
+    else if (arg > this.high[1]) return false
+  }
+  return true
+}
+
+function __regex(arg) {
+  return this.regex.test(arg)
+}
+
+function __typeof_length(arg) {
+  return __typeof.call(this, arg) && __length.call(this, arg)
+}
+
+function __typeof_regex(arg) {
+  return __typeof.call(this, arg) && __regex.call(this, arg)
+}
+
+function __typeof_length_regex(arg) {
+  return __typeof_length.call(this, arg) && __regex.call(this, arg)
+}
+
+function __ctype(arg) {
+  return arg instanceof this.ctype
+}
+  
+function _checkRecursive(args, list, array) {
+  if (!args.length) {
+    // no more args, check if the remaining list is required
+    if (!list.length) {
+      return true // no more list
+    } else if (list[0].required) {
+      return false // current is required
+    } else {
+      array.push(list[0].default)
+      return _checkRecursive(args, list.slice(1), array) // keep checking
+    }
+  }
+  if (!list.length) {
+    return true
+  }
+  // check if current is a match
+  if (list[0].match(args[0])) {
+    // it is, continue recursing
+    var arr = array.length
+    array.push(args[0])
+    if (_checkRecursive(args.slice(1), list.slice(1), array)) {
+      return true
+    }
+    array.splice(arr) // remove any potential added elements
+  }
+  // no match, is it required? if not, skip
+  if (!list[0].required) {
+    array.push(list[0].default)
+    return _checkRecursive(args, list.slice(1))
+  }
+  return false
 }
 
 function argtype(thisObj, args, func) {
-  if (arguments.length < 3) {
-    throw new Error('Argtype: Needs 3 arguments.')
-  }
-  if (typeof thisObj != 'object') {
-    throw new Error('Argtype: First argument must always be' +
-                    ' an object, array or null (global).')
-  }
-  if (args instanceof Function && func instanceof Array) {
-    // this is acceptable input (don't want to force one particular order).
-    // both the normal closure "convention"
-    //  argtype(this, [args], function(...) {...})
-    // and the nicer looking (if your func has a name already)
-    //  argtype(this, someFunc, [args])
-    // are acceptable
-    var tmp = args
-    args = func
-    func = tmp
-  }
-  if (!(args instanceof Array) || !(func instanceof Function)) {
-    throw new Error('Argtype: Must have one function and one array as' +
-                    ' arguments (order is irrelevant).')
-  }
-  return function() {
-    var argslist = []
-      , i = 0
-    for (var a = 0; a < args.length; a++) {
-      // if the argument is square bracketed (array), it's optional
-      var optional = args[a] instanceof Array
-        , type = optional ? args[a][0] : args[a]
-      if (!argtypeCheck(arguments[i], type)) {
-        // wrong type
-        if (!optional) {
-          // has wrong type or is missing, so we complain!
-          throw new Error('Function argument ' + (i + 1) +
-                          ' got ' + (typeof arguments[i]) +
-                          ', expected ' + type + '.')
-        }
-        // set its default value (or undefined, by design)
-        argslist.push(args[a][1])
-      } else {
-        // correct type
-        argslist.push(arguments[i++])
+  var a = 0
+    , len = args.length
+    , argslist = []
+    , current = -1
+    , ptr
+    , re = /^(\?)?(0)?(\+)?([\.nifsbcoat])(?:\=(\(|\[)(\-?(\d+(?:.\d+)?)?)\,(\-?(\d+(?:.\d+)?)?)(\)|\])|((?:\>|\<)\=?)(\-?\d+(?:.\d+)?)|\/(.*))?$/i
+    , matches = null
+    
+  // Parse the arguments list
+  for (; a <= len ; a++) {
+    if (typeof args[a] == 'string' && (matches = args[a].match(re))) {
+      // Does the string have proper argtype format?
+      if (matches === null) {
+        throw new SyntaxError('Argtype argument ' + a +
+                              ' is not a valid argtype string')
       }
+      
+      // Add this new argument
+      argslist.push({
+        required: !matches[1],
+        notnull: !matches[2],
+        type: matches[4]
+      })
+      
+      // Advance pointer
+      ptr = argslist[++current]
+      
+      // Regex
+      if (matches[11]) {
+        ptr.regex = new RegExp(matches[11], matches[3] ? undefined : 'i')
+      }
+      
+      // Range
+      var operator = {
+          '(': 'gt'
+        , '[': 'gte'
+        , ')': 'lt'
+        , ']': 'lte'
+        , '>': 'gt'
+        , '>=': 'gte'
+        , '<': 'lt'
+        , '<=': 'lte'}
+      if (matches[5]) {
+        if (matches[6]) ptr.low = [operator[matches[5]], parseFloat(matches[6])]
+        if (matches[7]) ptr.high = [operator[matches[8]], parseFloat(matches[7])]
+      }
+      if (matches[9]) {
+        if (matches[9] == '>' || matches[9] == '>=') {
+          ptr.low = [operator[matches[9]], parseFloat(matches[10])]
+        } else {
+          ptr.high = [operator[matches[9]], parseFloat(matches[10])]
+        }
+      }
+      
+      // Type specifics
+      switch (ptr.type) {
+        case '.': // No type check
+          ptr.match = ptr.regex
+                      ? __regex
+                      : function(){return true}
+          break
+        case 'n': // Number (integer or float)
+          ptr.ctype = 'number'
+          ptr.match = (ptr.low || ptr.high)
+                      ? __typeof_length
+                      : __typeof 
+          break
+        case 'i': // Integer
+          ptr.ctype = 'number'
+          ptr.regex = /^\-?\d+$/
+          ptr.match = (ptr.low || ptr.high)
+                      ? __typeof_length_regex
+                      : __typeof_regex
+          break
+        case 'f': // Float/double
+          ptr.ctype = 'number'
+          ptr.regex = /^\-?\d+\.\d+$/
+          ptr.match = (ptr.low || ptr.high)
+                      ? __typeof_length_regex
+                      : __typeof_regex
+          break
+        case 's': // String
+          ptr.ctype = 'string'
+          ptr.match = ptr.regex
+                      ? (ptr.low || ptr.high)
+                        ? __typeof_length_regex
+                        : __typeof_regex
+                      : (ptr.low || ptr.high)
+                        ? __typeof_length
+                        : __typeof
+          break
+        case 'b': // Boolean
+          ptr.ctype = 'boolean'
+          ptr.match = __typeof
+          break
+        case 'c': // Function: same as 't' with Function specified, although
+                  // uses typeof 'function' rather than instanceof Function
+          ptr.ctype = 'function'
+          ptr.match = __typeof
+          break
+        case 'o': // Object: same as 't' with Object specified, although
+                  // uses typeof 'object' rather than instanceof Object
+                  // Matches initialized objects only, not function-objects
+          ptr.ctype = 'object'
+          ptr.match = __typeof
+          break
+        case 'a': // Array: same as 't' with Array specified, but lets us skip the object argument.
+          ptr.ctype = Array
+          ptr.match = __ctype
+          break
+        case 't': // Function, Date, RegExp, etc, or even a custom type. Defaults to Object
+          ptr.ctype = Object
+          ptr.match = __ctype
+          break
+      }
+    } else if (typeof args[a] == 'function') {
+      if (~current) ptr = argslist[current]
+      else throw TypeError('Argtype element ' + a +
+                           'was not of expected type')
+      // If the current arg is a 't'-type this is either a default or a type
+      if (ptr.type == 't') ptr.ctype == Function ? ptr.default = args[a] : ptr.ctype = args[a]
+      // However, if it's a 'c'-type or untyped then this can only be a default
+      else if (ptr.type == 'c' || ptr.type == '.') ptr.default = args[a]
+    } else if (argslist[current].match(args[a])) {
+      argslist[current].default = args[a]
+    } else {
+      throw new TypeError('Argtype element ' + a +
+                          'was not of expected type')
     }
-    // concatenate any arguments beyond the type check
-    argslist = argslist.concat(Array.prototype.slice.call(arguments, i))
-    func.apply(thisObj, argslist)
+  }
+  
+  delete a, len, current, ptr, re, matches, args
+  
+  return function() {
+    var array = []
+    if (_checkRecursive(arguments.slice(0), argslist, array)) {
+      return func.apply(thisObj, array)
+    }
+    throw new TypeError('Argument missing or wrong type')
   }
 }
 
-// for CommonJS use, we export the function
-// browsers will (generally) ignore, just make sure you load this file first
-// if you at any point specify a global module object yourself
-if (module) {
-  module.exports = argtype
-}
+// Yo dawg...
+argtype = argtype(module, ['?o', null, 'a>0', 'c'], argtype)
+module.exports = argtype
